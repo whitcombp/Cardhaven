b062b71fe6fd3977354bb931cddc2ff51512bf4d
whitcombp <whitcombp@msoe.edu>
2026-01-31 14:37:46 -0600

    spellweaver support added

--- get_cards_json.py
+++ get_cards_json.py
@@ -1,36 +1,30 @@
 import os
 import json
 
-character = 'doomstalker'
+character = "spellweaver"
 
 cards_json = []
 
 # Read in favorites (if they exist)
 favorites = set()
-if os.path.exists('favorites.txt'):
-    with open('favorites.txt', 'r') as file:
+if os.path.exists("favorites.txt"):
+    with open("favorites.txt", "r") as file:
         [favorites.add(f.strip().lower()) for f in file]
-print('favorites:', favorites)
+print("favorites:", favorites)
 
-path = os.path.join('images', character, 'ability_cards')
-dir_walk = os.walk(path) # dirpath (str), dirnames (list), filenames (list)
+path = os.path.join("images", character, "ability_cards")
+dir_walk = os.walk(path)  # dirpath (str), dirnames (list), filenames (list)
 levels = next(dir_walk)[1]
 for dir, level in zip(dir_walk, levels):
     for filename in dir[2]:
-        name = filename.split('.')[0].replace('-', ' ')
+        name = filename.split(".")[0].replace("-", " ")
         card = {
-            "filename" : os.path.join(dir[0], filename), # full file name
-            "name" : name.__add__(level) # add for level filtering
-                ,
-            "level" : level,
-            "favorite" : name in favorites 
+            "filename": os.path.join(dir[0], filename),  # full file name
+            "name": name.__add__(level),  # add for level filtering
+            "level": level,
+            "favorite": name in favorites,
         }
         cards_json.append(card)
 
-with open("cards.json", 'w') as file:
-    json.dump(
-        cards_json,
-        file,
-        indent=4
-    )
-
+with open("cards.json", "w") as file:
+    json.dump(cards_json, file, indent=4)
--- get_flips_json.py
+++ get_flips_json.py
@@ -1,24 +1,16 @@
 import os
 import json
 
-character = 'doomstalker'
+character = "spellweaver"
 
 flips_json = []
 
 # Read in the modifiers for selected character
-modifiers_path = os.path.join('images', character, 'attack_modifiers')
-modifiers = [os.path.join(modifiers_path, m)
-              for m in os.listdir(modifiers_path)]
+modifiers_path = os.path.join("images", character, "attack_modifiers")
+modifiers = [os.path.join(modifiers_path, m) for m in os.listdir(modifiers_path)]
 
 # Update for if the modifier needs a reshuffle (natural whiff or double damage)
-modifiers = [{'card' : m,
-              'reshuffle' : 'x2' in m or 'x0' in m}
-              for m in modifiers]
+modifiers = [{"card": m, "reshuffle": "x2" in m or "x0" in m} for m in modifiers]
 
-with open("flips.json", 'w') as file:
-    json.dump(
-        modifiers,
-        file,
-        indent=4
-    )
-
+with open("flips.json", "w") as file:
+    json.dump(modifiers, file, indent=4)
